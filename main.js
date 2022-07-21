10c51b75530be52f59da31afb48558a6cdd59974
Karim Korgoloev @Mac <korgoloevk@gmail.com>
2022-07-21 14:36:11 +0300

    step1 final

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const body = document.querySelector("body");
 const range = document.querySelector(".range");
 const gameSection = document.querySelector(".gameSection");
+const rangeBlock = document.querySelector(".rangeBlock");
+const count = document.querySelector(".count");
 
-range.style.width = gameSection.clientHeight - 60 + "px";
-body.style.height = window.onresize = () => {
-  range.style.width = gameSection.clientHeight - 60 + "px";
+range.style.width = rangeBlock.clientHeight - 60 + "px";
+window.onresize = () => {
+  range.style.width = rangeBlock.clientHeight - 60 + "px";
 };
+
+range.addEventListener("change", () => {
+  count.textContent = "Счёт " + range.value;
+});
 
 console.log(gameSection.clientHeight);
