945342d93139768e02461b0dbaf0e43816cc628d
Karim Korgoloev @Mac <korgoloevk@gmail.com>
2022-07-21 12:37:48 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const range = document.querySelector(".range");
+const gameSection = document.querySelector(".gameSection");
+
+range.style.width = gameSection.clientHeight - 60 + "px";
+
+window.onresize = () => {
+  range.style.width = gameSection.clientHeight - 60 + "px";
+};
+
+console.log(gameSection.clientHeight);
